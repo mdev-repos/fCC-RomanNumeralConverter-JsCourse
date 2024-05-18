0e7e075f9eb7e39af4259b8c4b467dc94f7c864d
mdev <168182203+mdev-repos@users.noreply.github.com>
2024-05-18 20:09:33 -0300

    Full Js logic working

--- app.js
+++ app.js
@@ -3,56 +3,49 @@ const convertBtn = document.getElementById("convert-btn");
 const outputContainer = document.getElementById("output");
 
 
+function convertToRoman(value){
+    const romanNum = [
+        ['', "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
+        ['', "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
+        ['', "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],
+        ['', "M", "MM", "MMM"]
+    ];
+    let numArr = String(value).split("").map((value) => { return Number(value) });
+    let numLeng = value.length - 1; var str = '';
+    for (let index = 0; index < value.length; index++) {
+        str += romanNum[numLeng][numArr[index]];
+        numLeng--;
+    };
+ return str;
+}
 
-convertBtn.addEventListener('click', ()=>{
-    /*/While div already have a p, erase p
+convertBtn.addEventListener('click', (e) => {
+    //Prevent default actions
+    e.preventDefault();
+    let value = userInput.value;
+
+    //Create a P element to show the results
     while(outputContainer.firstChild){
         outputContainer.removeChild(outputContainer.firstChild);
+        outputContainer.classList.toggle("hidden");
     }
-
-    outputElement = document.createElement("p");
-
-    switch(userInput.value){
-
-    }
-    */
-   console.log(userInput.value);
-})
-
-
-/*
-const resultElement = document.createElement("p");
-
-
-//Functions
-
-function inputValidation(inputValue){
-
-    resultElement.innerText = '';
+    const outputElement = document.createElement("p");
+    outputContainer.classList.toggle("hidden");
 
     //Validation paths
-    if(inputValue==''){
-        resultElement.innerText = "Please enter a valid number";
-    }
-    else if(inputValue<=0){
-        resultElement.innerText = "Please enter a number greater than or equal to 1";
-    }
-    else if(inputValue=4000){
-        resultElement.innerText = "Please enter a number less than or equal to 3999";
-    }
-    else{
-        convertToRoman();
-    }
+   if(value == ''){
+    outputElement.textContent = "Please enter a valid number";
+   }
+   else if(value <= 0){
+    outputElement.textContent = "Please enter a number greater than or equal to 1";
+   }
+   else if(value >= 4000){
+    outputElement.textContent = "Please enter a number less than or equal to 3999";
+   }
+   else{
+    outputElement.textContent = `${convertToRoman(value)}`;
+   }
 
-    outputContainer.appendChild(resultElement);
-}
-
-function convertToRoman(value){
-
-}
-
-
-//Event handler
-convertBtn.addEventListener('click', inputValidation(userInput.value));
-
-*/
+   //Add the outputElement to outputContainer
+   outputContainer.appendChild(outputElement);
+});
